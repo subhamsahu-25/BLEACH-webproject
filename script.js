52b96072a1791b4bdf32382c64a06b36a79348a8
SHUBH <sahusubham715@gmail.com>
2026-02-04 09:59:53 +0530

    Wrap functions in DOMContentLoaded event listener

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-function visible(imageBankai, audioBankai, reishiColor1, reishiColor2, reishiColor3) 
-{
-   document.getElementById('reveal').style.display = 'flex';
-   const charBleach = document.getElementById('charBleach');
-   charBleach.src = imageBankai;
-
-   charBleach.style.filter = `drop-shadow(0 0 6px ${reishiColor1}) drop-shadow(0 0 16px ${reishiColor2}) drop-shadow(0 0 32px ${reishiColor3})`;
-
-   const audio = new Audio(audioBankai);
-   setTimeout(() => {
-      audio.play();
-   }, 1000);
-}
-function invisible() 
-{
-   document.getElementById('reveal').style.display = 'none';
-}
+document.addEventListener("DOMContentLoaded", () => {
+   function visible(imageBankai, audioBankai, reishiColor1, reishiColor2, reishiColor3) 
+   {
+      document.getElementById('reveal').style.display = 'flex';
+      const charBleach = document.getElementById('charBleach');
+      charBleach.src = imageBankai;
+   
+      charBleach.style.filter = `drop-shadow(0 0 6px ${reishiColor1}) drop-shadow(0 0 16px ${reishiColor2}) drop-shadow(0 0 32px ${reishiColor3})`;
+   
+      const audio = new Audio(audioBankai);
+      setTimeout(() => {
+         audio.play();
+      }, 1000);
+   }
+   function invisible() 
+   {
+      document.getElementById('reveal').style.display = 'none';
+   }
+})
